7a59d5d3115fdf115fcb43756b0412acfc294dba
benedictnkosi <nkosi.benedict@gmail.com>
2025-02-20 21:01:32 +0200

    Initialize learner streak to zero on new learner creation
    - Add default streak initialization when creating a new Learner entity
    - Set initial streak value to 0 during learner registration
    - Ensure consistent streak tracking for new learners

--- src/Service/LearnMzansiApi.php
+++ src/Service/LearnMzansiApi.php
@@ -565,6 +565,7 @@ class LearnMzansiApi extends AbstractController
             if (!$learner) {
                 $learner = new Learner();
                 $learner->setUid($uid);
+                $learner->setStreak(0);
                 $learner->setCreated(new \DateTime());
                 $this->em->persist($learner);
                 $this->em->flush();
