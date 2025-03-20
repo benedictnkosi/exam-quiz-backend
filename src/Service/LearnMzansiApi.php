1b06aba3a449a106ed00273b18ddb5906f8d043f
nkosibenedict <nkosi.benedict@gmail.com>
2025-03-20 16:49:50 +0200

    Remove user exemption from new questions limit in LearnMzansiApi
    - Updated the validation logic to apply the new questions limit uniformly, removing the previous exemption for 'Mnelisi Mdletshe'.
    - This change ensures that all users are subject to the same approval process when submitting new questions, maintaining consistency in question management.

--- src/Service/LearnMzansiApi.php
+++ src/Service/LearnMzansiApi.php
@@ -142,7 +142,7 @@ class LearnMzansiApi extends AbstractController
 
             //return an error if the capturer has more than 50 new questions
             $newQuestions = $this->em->getRepository(Question::class)->findBy(['capturer' => $user->getId(), 'status' => 'new']);
-            if (count($newQuestions) >= 50 && $questionId == 0 && $user->getName() !== 'Mnelisi Mdletshe') {
+            if (count($newQuestions) >= 50 && $questionId == 0) {
                 return array(
                     'status' => 'NOK',
                     'message' => 'You have more than 50 new questions - Please approve your new questions'
