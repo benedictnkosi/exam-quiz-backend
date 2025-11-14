1ade6da0609f1241c5f2ffc74ea64eb0d2c85640
benedictnkosi <nkosi.benedict@gmail.com>
2025-11-14 15:35:46 +0200

    Update voice ID in run-daily-news script for YouTube uploads

--- scripts/run-daily-news.sh
+++ scripts/run-daily-news.sh
@@ -6,4 +6,4 @@ set -euo pipefail
 
 CONSOLE="$(dirname "$0")/../bin/console"
 
-"$CONSOLE" app:sabc-past-four-hours-direct --no-burn --avatar-id=93bf36d167184854bdde4ffb3b340981 --voice-id=24f17f42c557477faa14c21920ad0713 --upload-youtube
+"$CONSOLE" app:sabc-past-four-hours-direct --no-burn --avatar-id=93bf36d167184854bdde4ffb3b340981 --voice-id=6e032ca64a1745a0b48036e64042b2fb --upload-youtube
